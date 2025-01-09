71d73ea1ad791768acd7032ec0d9ac973d64a5d0
Alexander Levin <alevin.ru@gmail.com>
2025-01-09 13:40:20 +0200

    eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config({ ignores: ['*.d.ts', '**/coverage', '**/dist'] }, {
+  extends: [eslint.configs.recommended, ...typescriptEslint.configs.recommended, ...eslintPluginVue.configs['flat/recommended']],
+  files: ['src/**/*.{ts,vue}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: globals.browser,
+    parserOptions: {
+      parser: typescriptEslint.parser,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/require-default-prop': 'off',
+  },
+}, eslintConfigPrettier);
